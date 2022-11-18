087e249fe3d2140d26e98e5f0b4e3614662949d1
LEEKYUNGJAE <zzrudow@naver.com>
2022-11-18 15:35:41 +0900

    v2

--- next_imc/com/Main1.js
+++ next_imc/com/Main1.js
@@ -4,7 +4,7 @@ export default function Main1(){
 
     return(
         <div style={{width:'1800px', margin:'auto'}}>
-            <img src={require('../images/images (2).png')} alt='logo_image' style={{marginTop:'30px'}} />     
+            <img src={'images/images.png'} alt='logo_image' style={{marginTop:'30px'}} />     
         </div>
     );
 }
