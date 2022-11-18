df74587ae4d404bd08c10e943efd6654694ad23e
DESKTOP-H1BCD4Q\Lee <cat0182@daum.net>
2022-11-18 12:23:51 +0900

    pro1

---
+++ boot_imc/src/main/java/com/iamcamper/boot_imc/BootImcApplication.java
@@ -0,0 +1,13 @@
+package com.iamcamper.boot_imc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootImcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootImcApplication.class, args);
+	}
+
+}
---
+++ boot_imc/src/main/java/com/iamcamper/boot_imc/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.iamcamper.boot_imc;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootImcApplication.class);
+	}
+
+}
