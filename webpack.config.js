66c10f567be0fcd4e40966d4f1f337c9fde36793
유라라랜드 <102152166+yuraraland@users.noreply.github.com>
2023-04-16 22:16:36 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry : './src/index.js',
   output : {
     filename : 'main.js',
-    path : '${__dirname}/dist',
+    path : path.resolve(__dirname, './dist'),
   },
   mode : 'development',
 }
