b3fde349ab7d5abf80007049f8e17a565ca28a0e
유라라랜드 <102152166+yuraraland@users.noreply.github.com>
2023-04-16 22:27:35 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     path : path.resolve(__dirname, './dist'),
   },
   mode : 'development',
+  loaders:[  {
+    test: /\.(gif|svg|jpg|png)$/,
+    loader: "file-loader",
+  }]
 }
+
+const withImages = require('next-images')
+
+module.exports = withImages();
